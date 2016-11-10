5c98c2333389b3325345b44e4e8c914c0a9605fe
break-thru <hyowon.lim@gmail.com>
2016-11-10 20:11:28 +0900

    Update hyowon_models.py

--- hyowon_models.py
+++ hyowon_models.py
@@ -12,3 +12,24 @@ class Attendance(models.Model):
     student = models.ForeignKey(Student)
     checked = models.BooleanField(default=False)
     date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        if self.checked:
+            attendance = "출석"
+        else:
+            attendance = "결석"
+        return self.student.name + ", " + attendance + ", " + str(self.date)
+
+
+    # 출석체크를 시작할 때: 등록된 학생 모두를 불러옴
+
+    def check_start():
+        student_list = Student.objects.all()
+        for student in student_list:
+            Attendance.objects.get_or_create(student=student, date=date.today())
+
+    # 출석을 처리할 때
+
+    def attend(self):
+        self.checked = True
+        self.save()
